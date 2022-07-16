2e90217a23d0e37d4bc721166261f4494e6861b5
Zain Ansari <zain.ansari@live.com>
2022-07-16 12:01:42 +0100

    get data from bootstrap form

--- static/index.js
+++ static/index.js
@@ -1,47 +1,5 @@
 consoleText(['choose an index', 'define a strategy', 'calculate your returns'], 'text',['darkseagreen','darksalmon','lightblue']);
 
-// var jsonfile = {
-//   "jsonarray": [{
-//      "name": "Joe",
-//      "age": 12
-//   }, {
-//      "name": "Tom",
-//      "age": 14
-//   }]
-// };
-
-// var labels = jsonfile.jsonarray.map(function(e) {
-//   return e.name;
-// });
-// var data = jsonfile.jsonarray.map(function(e) {
-//   return e.age;
-// });;
-
-// var config = {
-//   type: 'line',
-//   data: {
-//      labels: labels,
-//      datasets: [{
-//         label: 'Graph Line',
-//         data: data,
-//         backgroundColor: 'rgba(0, 119, 204, 0.3)'
-//      }]
-//   },
-//   options: {
-//     layout: {
-//       padding: {
-//         left: 50,
-//         right: 50,
-//         top: 25,
-//         bottom: 25
-//       }
-//     }
-//   }
-// };
-
-// var chart = new Chart(document.getElementById('myChart'), config);
-
-// function([string1, string2],target id,[color1,color2])    
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ['#fff'];
   var visible = true;
